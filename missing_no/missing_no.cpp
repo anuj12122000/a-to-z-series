4a41d8edbc6ab8b11cf7e05132e412886d2487a0
anuj12122000 <62623809+anuj12122000@users.noreply.github.com>
2025-01-29 00:33:43 +0530

    added the problem link

--- missing_no/missing_no.cpp
+++ missing_no/missing_no.cpp
@@ -1,9 +1,13 @@
 
-// rather than doing n*(n-1)/2 ;
+// rather than doing n*(n+1)/2 ;
 
 // i first did the xor from all the aelements from 1 to N 
 // and then i did the xor of all the elements presents in the array 
 // as xor of same elements would cancel out each other , I would be automatically left with the missing elemment
+
+
+// question link : 
+// https://www.naukri.com/code360/problems/missing-number_6680467?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_Arrayproblems
 
 int missingNumber(vector<int>&a, int N) {
     int withOutMissingXor = {};
